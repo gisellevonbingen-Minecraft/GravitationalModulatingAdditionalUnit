518b4bf4a1c5eb1205e4911e8ded22de80309bbd
Giselle <gisellevonbingen@gmail.com>
2025-08-10 11:10:49 +0900

    Update GMUTKeyHandler.java

--- src/main/java/giselle/gmut/client/key/GMUTKeyHandler.java
+++ src/main/java/giselle/gmut/client/key/GMUTKeyHandler.java
@@ -7,7 +7,6 @@ import giselle.gmut.common.GMUTLang;
 import giselle.gmut.common.content.gear.mekasuit.EntityModuleHelper;
 import giselle.gmut.common.content.gear.mekasuit.ModuleGravitationalModulatingAdditionalUnit;
 import giselle.gmut.common.network.to_server.PacketSwitchVerticalSpeedPacket;
-import giselle.gmut.common.registries.GMUTModules;
 import mekanism.api.gear.IModule;
 import mekanism.client.ClientRegistrationUtil;
 import mekanism.client.key.MekKeyBindingBuilder;
